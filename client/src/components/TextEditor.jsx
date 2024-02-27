161cfcae6fcde949e33216b457065b746ad88721
Divy <divyparekh1810@gmail.com>
2024-02-27 22:22:24 +0530

    done setting up web-sockets

---
+++ client/src/components/TextEditor.jsx
@@ -0,0 +1,61 @@
+import { useEffect, useRef, useState } from "react";
+import Quill from "quill";
+import "quill/dist/quill.snow.css";
+
+import { io } from "socket.io-client";
+
+const TextEditor = () => {
+  const [socket, setSocket] = useState();
+  const [quill, setQuill] = useState();
+  const wrapperRef = useRef();
+  const editorRef = useRef(null);
+
+  useEffect(() => {
+    if (!editorRef.current) {
+      const editor = document.createElement("div");
+      wrapperRef.current.append(editor);
+      const q = new Quill(editor, { theme: "snow" });
+      editorRef.current = q;
+      setQuill(q);
+    }
+  }, []);
+
+  useEffect(() => {
+    const s = io("http://localhost:3001");
+    setSocket(s);
+    return () => {
+      s.disconnect();
+    };
+  }, []);
+
+  useEffect(() => {
+    if (socket == null || quill == null) return;
+    const handler = (delta, oldData, source) => {
+      if (source !== "user") return;
+      socket?.emit("send-changes", delta);
+    };
+
+    quill?.on("text-change", handler);
+
+    return () => {
+      quill?.off("text-change", handler);
+    };
+  }, [socket, quill]);
+
+  useEffect(() => {
+    if (socket == null || quill == null) return;
+    const handler = (delta) => {
+      quill?.updateContents(delta);
+    };
+
+    socket?.on("receive-changes", handler);
+
+    return () => {
+      socket?.off("receive-changes", handler);
+    };
+  }, [socket, quill]);
+
+  return <div className="container" ref={wrapperRef}></div>;
+};
+
+export default TextEditor;
